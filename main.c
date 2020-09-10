af863a83ec6241fd771b2f5b8d93fef631a0b6e3
smoir <moise605@student.otago.ac.nz>
2020-09-10 21:02:30 +1200

    added cmd arg parsing skeleton

--- main.c
+++ main.c
@@ -1,29 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
 
 #include "tree.h"
+#include "mylib.h"
 
 static void print_key(int freq, char *word)
 {
     printf("%-4d %s\n", freq, word);
-
 }
 
-int main()
+int main(int argc, char **argv)
 {
-    tree b = tree_new(RBT);
-    char s[256];
-    while (1 == scanf("%255s", s))
+    char word[256];
+    char option;
+    const char *optstring = "c:df:orh";
+    tree t = tree_new(RBT);
+    
+    while ((option = getopt(argc, argv, optstring)) != EOF)
     {
-        b = tree_insert(b, s);
+        switch (option)
+        {
+        case 'c':
+            /* spell checker */
+            break;
+        case 'd':
+            /* print tree depth */
+            break;
+        case 'f':
+            /* do something else */
+            break;
+        case 'o':
+            /* output tree in DOT form */
+            break;
+        case 'r':
+            /* use RBT */
+            break;
+        case 'h':
+            /* help menu */
+            break;
+        default:
+            /* if an unknown option is given */
+            break;
+        }
     }
-    printf("INORDER\n");
-    tree_inorder(b, print_key);
-    printf("\n\n");
-    printf("PREORDER\n");
-    tree_preorder(b, print_key);
-    printf("\nTREE DEPTH====\n%d\n", tree_depth(b));
 
-    tree_free(b);
+    while (getword(word, sizeof word, stdin) != EOF)
+    {
+        t = tree_insert(t, word);
+    }
     return 0;
 }
