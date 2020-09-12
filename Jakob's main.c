9354f76250b8b321decf7ae16a68e97e93459595
tournageCoding <64759337+tournageCoding@users.noreply.github.com>
2020-09-12 14:08:31 +1200

    Update Jakob's main.c

--- Jakob's main.c
+++ Jakob's main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <getopt.h>
 #include <time.h>
+#include <string.h>
 
 #include "tree.h"
 #include "mylib.h"
@@ -10,38 +11,32 @@ static void print_info(int freq, char *word) {
     printf("%-4d %s\n", freq, word);
 }
 
-static void print_key(int freq, char *word) {
-    printf("%s\n", word);
-}
-
 int main(int argc, char *argv[]) {
 
     const char *optstring = "c:df:orh";
     char option;
     char *input;
+    char *f_filename = "";
     tree t;
     int c_option = 0, d_option = 0, f_option = 0,
         o_option = 0, r_option = 0, h_option = 0;
     int unknown_words;
     FILE *c_file;
-    FILE *f_file;
-    FILE *d_file;
+    FILE *o_file;
     clock_t fill_start, fill_end, search_start, search_end;
 
     while ((option = getopt(argc, argv, optstring)) != EOF) {
         switch (option) {
             case 'c':
-                /* Safely open file*/
-                if (NULL == (c_file = fopen(optarg, "r"))) {
-                    fprintf(stderr, "Can't find file '%s'\n", optarg);
-                    return EXIT_FAILURE;
-                }
+                c_file = open_file(optarg, "r");
                 c_option = 1;
                 break;
             case 'd':
                 d_option = 1;
                 break;
             case 'f':
+                f_filename = emalloc((strlen(optarg) + 1) * sizeof f_filename);
+                strcpy(f_filename, optarg);
                 f_option = 1;
                 break;
             case 'o':
@@ -65,31 +60,18 @@ int main(int argc, char *argv[]) {
         t = tree_new(RBT);
     }
 
-    /* If -c is given, ignore -o and -d */
-    if (c_option == 1) {
-        d_option = 0;
-        o_option = 0;
-    }
-
     /* Read stdin into the tree */
-    
-
-    if (h_option == 1) {
-        printf("Help message describing how to use the program.\n");
+    input = emalloc(100 * sizeof input);
+    fill_start = clock();
+    while (getword(input, 99, stdin) != EOF) {
+        t = tree_insert(t, input);
     }
+    fill_end = clock();
 
-    if (c_option == 1) {
-        input = emalloc(100 * sizeof input);
+    /* option c*/
+    if (c_option == 1) {      
         unknown_words = 0;
 
-        /* Insert stdin to tree*/
-        fill_start = clock();
-        while (getword(input, 99, stdin) != EOF) {
-            t = tree_insert(t, input);
-        }
-        fill_end = clock();
-
-        /* Read each word from filename*/
         search_start = clock();
         while (getword(input, 99, c_file) != EOF) {
             if (tree_search(t, input) == 0) {
@@ -100,14 +82,16 @@ int main(int argc, char *argv[]) {
         search_end = clock();
 
         fclose(c_file);
-        free(input);
 
         fprintf(stderr, "Fill time\t: %f\n",
                 (fill_end - fill_start) / (double)CLOCKS_PER_SEC);
         fprintf(stderr, "Search time\t: %f\n",
                 (search_end - search_start) / (double)CLOCKS_PER_SEC);
         fprintf(stderr, "Unknown words = %d\n", unknown_words);
-        
+
+        /* If -c is given, ignore -d and -o*/
+        d_option = 0;
+        o_option = 0;        
     }
 
     if (d_option == 1) {
@@ -118,26 +102,13 @@ int main(int argc, char *argv[]) {
     }
 
     if (o_option == 1) {
-        /* note: you can use
-           dot -Tpdf < tree-view.dot > tree-view.pdf
-           in terminal to check out the file */
-        input = emalloc(100 * sizeof input);
-        
-        /* Insert stdin to tree*/
-        while (getword(input, 99, stdin) != EOF) {
-            t = tree_insert(t, input);
-            
+        if (f_option == 0) {
+            f_filename = "tree-view.dot";
         }
-
-        /* FILE */
-        
-        d_file = fopen("./tree-view.dot", "w");
-        tree_output_dot(t, d_file);
-
-        
-        if (f_option == 1) {           
-        } else {
-        }
+        o_file = open_file(f_filename, "w");
+        printf("Creating dot file '%s'\n", f_filename);
+        tree_output_dot(t, o_file);
+        fclose(o_file);
     }
 
     if (h_option == 1) {
@@ -150,6 +121,7 @@ int main(int argc, char *argv[]) {
         printf("-h\n");
     }
 
+    free(input);
     tree_free(t);
     
     return EXIT_SUCCESS;
